03c408d8b5fbe46ebedeac93fcf4203184588481
viimamura <vitoria.imamura@hotmail.com>
2025-04-05 00:32:43 -0300

    Criar telas principais

---
+++ src/app/features/agendamentos/views/agendamentos/agendamentos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendamentos',
+  templateUrl: './agendamentos.component.html',
+  styleUrls: ['./agendamentos.component.scss']
+})
+export class AgendamentosComponent {
+
+}
