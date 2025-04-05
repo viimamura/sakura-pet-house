6aa00749fb59d38a60f7ba39c1078d44748efc90
viimamura <vitoria.imamura@hotmail.com>
2025-04-05 00:34:59 -0300

    Atualizar rota principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'pets', loadChildren: () => import('./features/pets/pets.module').then(m => m.PetsModule) }, { path: 'agendamentos', loadChildren: () => import('./features/agendamentos/agendamentos.module').then(m => m.AgendamentosModule) }];
+const routes: Routes = [
+  {
+    path: 'pets',
+    loadChildren: () =>
+      import('./features/pets/pets.module').then(m => m.PetsModule)
+  },
+  {
+    path: 'agendamentos',
+    loadChildren: () =>
+      import('./features/agendamentos/agendamentos.module').then(m => m.AgendamentosModule)
+  },
+  { path: '', redirectTo: 'pets', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pets' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
